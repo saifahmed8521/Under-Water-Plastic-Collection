e465aba740786e88cffc5413f3e82b3e26a03f3e
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-03-31 04:53:27 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # Introduction:
-st.title("Welcom to [OurProject Name]")
+st.title("Welcom to SeaRaptor")
 st.header('Why we need to clean :blue[Oceans] :')
 st.markdown("We need to clean the ocean because, well, it's where all the cool sea creatures live, and we can't let them swim in a garbage dump! Plus, nobody wants to surf on a plastic wave or have their beach vacation ruined by a plastic picnic. Let's tidy up the ocean and give marine life the clean, blue playground they deserve! 🌊🧹")
 st.image('cleaning-the-ocean.jpg')
