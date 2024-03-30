a012948c27f1c85f4064f03b3cee809d840d9ed6
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-03-30 21:08:37 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,14 @@
 import streamlit as st
-
-st.title('Why we need to clean :blue[Oceans] :')
+# Introduction:
+st.title("Welcom to [OurProject Name]")
+st.header('Why we need to clean :blue[Oceans] :')
 st.markdown("We need to clean the ocean because, well, it's where all the cool sea creatures live, and we can't let them swim in a garbage dump! Plus, nobody wants to surf on a plastic wave or have their beach vacation ruined by a plastic picnic. Let's tidy up the ocean and give marine life the clean, blue playground they deserve! 🌊🧹")
 st.image('cleaning-the-ocean.jpg')
+
+st.title(":violet[Our] Mission")
+st.markdown("Our journey begins where life itself started: in the ocean. We're on a mission to clean up our very first home. With love and determination, we're removing plastic to create a safe haven where marine life can thrive and our planet can flourish.")
+st.image('fishes.jpg')
+
+st.title(":red[Why] It Matters")
+st.markdown("It matters because our oceans are suffocating under a deluge of plastic, a crisis that threatens the very essence of life on Earth. From majestic marine mammals to delicate coral reefs, no corner of our blue planet is spared. Yet, in this dire struggle, lies a chance for redemption. By confronting this crisis head-on, we're not just saving marine life; we're rewriting the narrative of our planet's future. It's a battle against time, against apathy, against the forces of destruction. But with every piece of plastic removed, we're breathing life back into the veins of our oceans, igniting a beacon of hope that echoes across generations. So, yes, it matters more than words can convey. It's the ultimate showdown between despair and redemption, and we're the protagonists in this epic tale of salvation.")
+
